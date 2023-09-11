9772bde8803c99b373b12f793d38dc1031479860
123456msk <898926534@qq.com>
2023-09-11 19:46:03 +0800

    modify:修改第一次作业第二小题代码

--- software-engineering/src/software_engineering_1/Demo02.java
+++ software-engineering/src/software_engineering_1/Demo02.java
@@ -1,12 +1,21 @@
 package software_engineering_1;
 
+import java.util.Scanner;
+
+import static java.lang.Math.max;
+
 public class Demo02 {
     public static void main(String[] args) {
-        int[] a = {1, -2, 3, 5, -1};
-        int max = -0x3f3f3f3f;
-        for (int i = 0;i < a.length;i ++){
-            if(max < a[i]) max = a[i];
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0;i < n;i ++) nums[i] = scanner.nextInt();
+
+        int mx = nums[0],s = 0;
+        for(int i = 0;i < n;i ++){
+            s = max(s + nums[i],nums[i]);
+            mx = max(mx,s);
         }
-        System.out.println(max);
+        System.out.println(mx);
     }
 }
