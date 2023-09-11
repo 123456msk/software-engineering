19ab7870af24546c5bc43de229db81aabdad0659
123456msk <898926534@qq.com>
2023-09-11 10:53:27 +0800

    modify:软件工程第一次作业第三小题

--- src/软件工程第一次作业/Demo03.java
+++ src/软件工程第一次作业/Demo03.java
@@ -5,13 +5,15 @@ package 软件工程第一次作业;
 
 public class Demo03 {
     public static void main(String[] args) {
+        int cnt = 0;
         for(int n = 2;n <= 20000;n ++){
             int i;
             for(i = 2;i < n;i ++){
                 if(n % i == 0) break;
             }
             if(i == n) {
-                System.out.print(n);
+                if(cnt ++  % 5 == 0) System.out.println();
+                System.out.print(n + " ");
             }
         }
     }
